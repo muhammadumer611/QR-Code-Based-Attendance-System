f18e1ffa70848dc0e5360715ec1bba9a1784d26e
muhammadumer611 <m.umerjanii007@gmail.com>
2026-08-26 22:44:02 +0500

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
